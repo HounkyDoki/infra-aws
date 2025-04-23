ff8bc510b2eae8f6725a0cac647c76893023bed5
Hounky Doki <hounkydoki@outlook.com>
2025-04-23 15:55:59 +0200

    Initial commit - Infra AWS with Terraform & Ansible

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "eu-west-3"
+}
+
+variable "ami_id" {
+  default = "ami-029b6e21d09465678"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "hounkydoki"
+}
+
+variable "your_ip_cidr" {
+  default = "213.221.128.52/32"
+}
